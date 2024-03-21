193f8feaf6ae97ad9c96813c59d9b505ed8e1dee
Ahmed Qala3awyi <a7medAlqal3awyi>
2024-03-22 00:35:30 +0200

    Invert values in a C++ program

---
+++ level1/Invert values.cpp
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+// My Solution
+#include <vector>
+
+std::vector<int> invert(std::vector<int> values)
+{
+
+    for (int i = 0; i < values.size(); i++)
+    {
+
+        values[i] = values[i] * -1;
+    }
+    return values;
+}
