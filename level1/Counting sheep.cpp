c81addbc19567be1c067abf434ecfcda9b03f306
Ahmed Qala3awyi <a7medAlqal3awyi>
2024-04-04 00:56:57 +0200

    Counting sheep  done

---
+++ level1/Counting sheep.cpp
@@ -0,0 +1,31 @@
+// Counting sheep...
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+//// My solution
+#include <vector>
+
+using namespace std; 
+
+int count_sheep(vector<bool> arr) 
+{
+    int count = 0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i] == true){
+            count++;
+        }
+    }
+  
+}
+
