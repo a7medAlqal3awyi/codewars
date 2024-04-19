c246290319cd7aa498db2eba45f57ad236b4c702
Ahmed Qala3awyi <a7medAlqal3awyi>
2024-04-19 06:04:33 +0200

    Rock Paper Scissors  done

---
+++ level1/Rock Paper Scissors.cpp
@@ -0,0 +1,40 @@
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// my solution
+
+#include <string>
+
+std::string rps(const std::string &p1, const std::string &p2)
+{
+    if (p1 == p2)
+        return "Draw!";
+    else if (p1 == "scissors" && p2 == "paper")
+        return "Player 1 won!";
+    else if (p1 == "paper" && p2 == "rock")
+        return "Player 2 won!";
+    else if (p1 == "rock" && p2 == "scissors")
+        return "Player 1 won!";
+    else
+        return "Player 2 won!";
+}
+
+// more clever solution
+#include <string>
+
+std::string rps(const std::string& p1, const std::string& p2)
+{
+    if(p1 == p2)
+      return "Draw!";
+    if( ( p1[0] == 's' && p2[0] == 'p' )||
+        ( p1[0] == 'p' && p2[0] == 'r' )||
+        ( p1[0] == 'r' && p2[0] == 's' ) )
+        return "Player 1 won!";
+    return "Player 2 won!";
+}
