bd92ed0edd4d9657184d8bf305bb83b8f44eb5eb
Ahmed Qala3awyi <a7medAlqal3awyi>
2024-04-20 08:57:55 +0200

    get character from ASCII Value  done

---
+++ level1/get character from ASCII Value.cpp
@@ -0,0 +1,16 @@
+// get character from ASCII Value
+// DESCRIPTION:
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+char get_char(int i) {
+    return i;
+ // or  return char(i);
+
+}
