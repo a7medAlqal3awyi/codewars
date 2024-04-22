aa4ee89f0aa21d3ca96fc5b96b4e474f7f994490
Ahmed Qala3awyi <a7medAlqal3awyi>
2024-04-22 06:53:35 +0200

    Get Planet Name By ID and You only need one - Beginner

---
+++ level1/Get Planet Name By ID.cpp
@@ -0,0 +1,51 @@
+// Get Planet Name By ID
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+
+// My solution
+#include <string>
+
+std::string get_planet_name(int id)
+{
+    std::string name;
+    switch (id)
+    {
+    case 1:
+        name = "Mercury";
+        break;
+    case 2:
+        name = "Venus";
+        break;
+    case 3:
+        name = "Earth";
+        break;
+    case 4:
+        name = "Mars";
+        break;
+    case 5:
+
+        name = "Jupiter";
+        break;
+    case 6:
+        name = "Saturn";
+        break;
+    case 7:
+        name = "Uranus";
+        break;
+    case 8:
+        name = "Neptune";
+        break;
+    }
+    return name;
+}
+// other solution
+#include <string>
+
+std::string get_planet_name(int id)
+{
+  std::string planets[] = {"","Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"};
+  return planets[id];
+}
