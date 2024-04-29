a0594791972c6040f1ae0ec5a00bd9402f842a05
Ahemd Khaled <a7medAlqal3awyi>
2024-04-29 09:22:32 +0300

    Grasshopper - Terminal game combat function

---
+++ level1/Grasshopper - Terminal game combat function.cpp
@@ -0,0 +1,8 @@
+// Grasshopper - Terminal game combat function
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+// my solution
+int combat(int health, int damage){
+  
+  return health>damage? health-damage:0;
+}
